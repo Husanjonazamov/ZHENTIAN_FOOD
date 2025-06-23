14cc1a26504dd73a8529c44718d4c53753060015
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-06-23 18:54:52 +0500

    New g4f version: 0.5.5.5 (current: 0.3.9.7) | pip install -U g4f

--- core/apps/api/admin/product.py
+++ core/apps/api/admin/product.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
-from unfold.admin import ModelAdmin
+from unfold.admin import ModelAdmin, TabularInline
 
-from core.apps.api.models import ProductModel
+from core.apps.api.models import ProductModel, ProductimageModel
+
+class ProductImageInline(TabularInline):
+    model = ProductimageModel
+    extra = 2 
+    
+
 
 
 @admin.register(ProductModel)
 class ProductAdmin(ModelAdmin):
     list_display = (
         "id",
-        "__str__",
+        "title",
+        "subtitle",
+        "category"
     )
+    list_display_links = ("title", )
+    inlines = [ProductImageInline]
